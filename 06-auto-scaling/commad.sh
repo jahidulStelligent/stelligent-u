0f59bb262fb5cd88dd4444a86643dfc43e5dd75a
Jahidul Islam <mdjahidul.islam@macusstg2508166.myfiosgateway.com>
2022-04-19 11:52:08 -0400

    Auto scaling 6.3.x

--- 06-auto-scaling/commad.sh
+++ 06-auto-scaling/commad.sh
@@ -33,6 +33,94 @@ aws cloudformation update-stack --stack-name jahidul-06-auto-sacling-lab-lab-6-1
 aws cloudformation update-stack --stack-name jahidul-06-auto-sacling-lab-lab-6-1-2 \
                                 --template-body file://auto_sacling_lab_6_1_4.yaml \
                                 --profile temp
-aws cloudformation update-stack --stack-name jahidul-06-auto-sacling-lab-lab-6-1-2 \
+aws cloudformation create-stack --stack-name jahidul-06-auto-sacling-lab-lab-6-2 \
                                 --template-body file://auto_sacling_lab_6_1_5.yaml \
+                                --profile temp
+
+aws cloudformation describe-stack-resources \
+--stack-name jahidul-06-auto-sacling-lab-lab-6-2 \
+--profile temp
+
+aws cloudformation describe-stack-resources \
+--stack-name jahidul-06-auto-sacling-lab-lab-6-2 \
+--output text \
+--query 'StackResources[0].PhysicalResourceId' \
+--profile temp
+
+aws autoscaling describe-auto-scaling-groups \
+    --auto-scaling-group-name jislam-ec2-asg-by-cf-2 \
+    --profile temp
+
+aws autoscaling describe-auto-scaling-groups \
+    --auto-scaling-group-name jislam-ec2-asg-by-cf-2 \
+    --output text \
+    --query 'AutoScalingGroups[0].Instances[0].InstanceId' \
+    --profile temp
+
+aws ec2 terminate-instances \
+        --instance-ids i-03fb4ae4bf97bf0d4 \
+        --profile temp
+
+aws cloudformation update-stack --stack-name jahidul-06-auto-sacling-lab-lab-6-2 \
+                                --template-body file://auto_sacling_lab_6_2_2.yaml \
+                                --profile temp
+
+aws autoscaling set-instance-health \
+    --instance-id i-073898ed7e4b2dac2 \
+    --health-status Unhealthy \
+    --profile temp
+
+aws autoscaling enter-standby \
+    --instance-ids i-0d1f05f17aec35950 \
+    --auto-scaling-group-name jislam-ec2-asg-by-cf-2 \
+    --should-decrement-desired-capacity \
+    --profile temp
+
+aws autoscaling describe-auto-scaling-instances --instance-ids i-0d1f05f17aec35950 --profile temp
+# Suspend Launch
+aws autoscaling suspend-processes --auto-scaling-group-name jislam-ec2-asg-by-cf-2 --scaling-processes Launch --profile temp
+# One instance in standby mode
+aws autoscaling enter-standby \
+    --instance-ids i-0390bc79a2a702413 \
+    --auto-scaling-group-name jislam-ec2-asg-by-cf-2 \
+    --should-decrement-desired-capacity \
+    --profile temp
+# Exit Standby (While Launch is disable)
+aws autoscaling exit-standby \
+    --instance-ids i-0390bc79a2a702413 \
+    --auto-scaling-group-name jislam-ec2-asg-by-cf-2 \
+    --profile temp
+# Response received: Cannot move instances out of Standby for AutoScalingGroup jislam-ec2-asg-by-cf-2 while the Launch process is suspended
+# Going to resume the Launch and try putting instance back in service
+aws autoscaling resume-processes \
+    --auto-scaling-group-name jislam-ec2-asg-by-cf-2 \
+    --scaling-processes Launch \
+    --profile temp
+aws autoscaling exit-standby \
+    --instance-ids i-0390bc79a2a702413 \
+    --auto-scaling-group-name jislam-ec2-asg-by-cf-2 \
+    --profile temp
+
+aws autoscaling exit-standby \
+    --instance-ids i-0d1f05f17aec35950 \
+    --auto-scaling-group-name jislam-ec2-asg-by-cf-2 \
+    --profile temp
+# Stress package in debian
+sudo apt-get update
+sudo apt-get install stress
+sudo stress --cpu 8 -v --timeout 120s
+
+
+aws cloudformation update-stack --stack-name jahidul-06-auto-sacling-lab-lab-6-2 \
+                                --template-body file://auto_sacling_lab_6_3_2.yaml \
+                                --profile temp
+
+ssh -i "mdjahidul-key-pair.cer" admin@ec2-54-204-226-220.compute-1.amazonaws.com
+
+aws cloudformation create-stack --stack-name jahidul-06-auto-sacling-lab-lab-6-3 \
+                                --template-body file://auto_sacling_lab_6_3_3.yaml \
+                                --profile temp
+
+aws cloudformation update-stack --stack-name jahidul-06-auto-sacling-lab-lab-6-3 \
+                                --template-body file://auto_sacling_lab_6_3_4.yaml \
                                 --profile temp
